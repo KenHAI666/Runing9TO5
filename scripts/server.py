29d018badcd9dcfa1cf8fe7a5831e44377771afa
KenHAI666 <126831414+KenHAI666@users.noreply.github.com>
2025-09-07 19:12:31 +0800

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import web2jekyll
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
